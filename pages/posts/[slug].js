e6c931ab869a289d416bf0cb662e46bdbdf8ce35
Adam Zajler <adamzajler01@gmail.com>
2022-03-07 08:34:21 +0100

    working version (almost)

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,36 +1,19 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
+import { useState, useEffect } from "react/cjs/react.development";
 import Router from "next/router";
 import fetcher from "../../lib/fetcher";
 import { GET_SINGLE_POST, GET_ALL_SLUGS } from "../../lib/wordpress/posts";
 import { useRouter } from "next/router";
 
 export default function single_post({ postData }) {
-	// const { id, title, content, slug } = postData.post;
-	const [loading, setLoading] = useState(false);
+	const { id, title, content, slug } = postData.post;
 	const router = useRouter();
-	Router.events.on("routeChangeStart", (url) => {
-		setLoading(true);
-	});
 
-	Router.events.on("routeChangeComplete", (url) => {
-		setLoading(false);
-	});
-	console.log(Router);
-	// if (!router.isFallback && !slug) {
-	// 	return <ErrorPage statusCode={404} />;
-	// }
-	return <>{loading ? <h1>LOADING</h1> : <h1>ZAŁADOWANE</h1>}</>;
-}
+	if (!router.isFallback && !slug) {
+		return <ErrorPage statusCode={404} />;
+	}
 
-export async function getStaticPaths() {
-	const response = await fetcher(GET_ALL_SLUGS);
-	const allPostsSlugs = response.posts.edges;
-
-	return {
-		paths: allPostsSlugs.map((singleSlug) => `/posts/${singleSlug.node.slug}` || []),
-		fallback: false,
-	};
+	return <>{router.isFallback ? <h1>Loading…</h1> : <h1>Załadowane</h1>} </>;
 }
 
 export async function getStaticProps({ params }) {
@@ -42,5 +25,15 @@ export async function getStaticProps({ params }) {
 		props: {
 			postData: data,
 		},
+	};
+}
+
+export async function getStaticPaths() {
+	const response = await fetcher(GET_ALL_SLUGS);
+	const allPostsSlugs = response.posts.edges;
+
+	return {
+		paths: allPostsSlugs.map((singleSlug) => `/posts/${singleSlug.node.slug}` || []),
+		fallback: true,
 	};
 }
